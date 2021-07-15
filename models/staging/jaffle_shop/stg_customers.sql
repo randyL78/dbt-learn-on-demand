206ee13444fd6ef1f3a1c04db66ae246639e2139
RandyL <randy.layne@teamtreehouse.com>
2021-07-15 16:16:03 +0000

    add simple data and schema tests

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,12 +1,6 @@
-with customers as (
+select
+    id as customer_id,
+    first_name,
+    last_name
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from test_randy.jaffle_shop.customers
-
-)
-
-select * from customers
+from test_randy.jaffle_shop.customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,13 +1,6 @@
-with orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from test_randy.jaffle_shop.orders
-
-)
-
-select * from orders
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from test_randy.jaffle_shop.orders
